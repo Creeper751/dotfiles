3e7333359d785c332f9641fc2092205752fbb961
Josh cmd-line <joshua.stoneham@gmail.com>
2023-04-04 17:38:23 -0400

    Switch to Astronvim

--- nvim/config/nvim/init.lua
+++ nvim/config/nvim/init.lua
@@ -1,40 +1,18 @@
-if vim.g.neovide then
-    -- Helper function for transparency formatting
-	local alpha = function()
-	return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
-	end
-	-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-	vim.g.neovide_transparency = 0.0
-	vim.g.transparency = 0.8
-    vim.g.neovide_background_color = "#0f1117" .. alpha()
-    vim.g.neovide_floating_blur_amount_x = 4.0
-    vim.g.neovide_floating_blur_amount_y = 4.0
-    vim.g.neovide_remember_window_size = true
-    vim.g.neovide_cursor_animation_length = 0.13
-    vim.g.neovide_cursor_animate_in_insert_mode = true
-    vim.g.neovide_cursor_animate_command_line = true
-    vim.g.neovide_cursor_vfx_mode = "torpedo"
-end
-require "core"
-
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
-
-if custom_init_path then
-  dofile(custom_init_path)
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
 end
 
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
-if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify("Error setting up colorscheme: " .. astronvim.default_colorscheme, "error")
+  end
 end
 
-vim.opt.rtp:prepend(lazypath)
-require "plugins"
-
-dofile(vim.g.base46_cache .. "defaults")
-require'luasnip'.filetype_extend("ruby", {"rails"})
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
